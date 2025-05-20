d23c81f1f86d66705aebb457bb8dfe4df884eae7
mrezas10 <m_r_saf@yahoo.com>
2025-05-20 14:50:03 -0700

    build

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.object.detection',
-  appName: 'Object Detection',
-  webDir: 'build'
-};
-
-export default config;
